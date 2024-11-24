ea0f05a9bff7a4f2fffe6f5ff05b8aab7d0e73c6
Tylar Murray <tylarmurray@usf.edu>
2024-11-24 09:01:43 -0500

    no more emails

--- r04winter/submit.sh
+++ r04winter/submit.sh
@@ -2,8 +2,6 @@
 #SBATCH --time=168:00:00
 #SBATCH --ntasks=4
 #SBATCH --mem-per-cpu=2000
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=tylarmurray@usf.edu
 #SBATCH --array=1-2
 #
 # slurm submit script
